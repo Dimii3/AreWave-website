cd7bc1f69b265dfb8a7355a13a7a2ca005fabcce
Dawid <dawid.marchewczyk@gmail.com>
2023-11-25 03:01:39 +0100

    accesibility fixed

--- js/main.js
+++ js/main.js
@@ -3,6 +3,11 @@ const navLinks = document.querySelectorAll('.nav-links__link');
 const navBtn = document.querySelector('.menu-btn');
 const animateEl = document.querySelectorAll('.animate');
 const footerYear = (document.querySelector('.year').textContent = new Date().getFullYear());
+
+const cookieModal = document.querySelector('.cookie-modal');
+cookieModal.showModal();
+const cookieBtn = document.querySelector('.cookie-btn');
+
 let isMenuOpen = false;
 const isVisible = () => {
 	const windowHeight = (window.innerHeight / 5) * 4;
@@ -17,23 +22,36 @@ const menuHandler = () => {
 	if (!isMenuOpen) {
 		navBtn.classList.add('active');
 		nav.classList.add('active');
-		document.body.style.overflow = 'hidden'
+		document.body.style.overflow = 'hidden';
 		isMenuOpen = true;
 	} else {
 		navBtn.classList.remove('active');
 		nav.classList.remove('active');
-		document.body.style.overflow = 'visible'
+		document.body.style.overflow = 'visible';
 		isMenuOpen = false;
 	}
 };
+
+const checkCookie = () => {
+	if (localStorage.getItem('cookie') === 'accept') {
+		cookieModal.close();
+	}
+};
+checkCookie();
 
 // LISTENERS
 window.addEventListener('scroll', () => {
 	isVisible();
 	window.scrollY > 200 ? nav.classList.add('expand') : nav.classList.remove('expand');
 });
-navLinks.forEach(link => link.addEventListener('click', () => {
-	menuHandler()
-	document.body.style.overflow = 'visible'
-}))
+navLinks.forEach((link) =>
+	link.addEventListener('click', () => {
+		menuHandler();
+		document.body.style.overflow = 'visible';
+	})
+);
 navBtn.addEventListener('click', menuHandler);
+cookieBtn.addEventListener('click', () => {
+	localStorage.setItem('cookie', 'accept');
+	cookieModal.close();
+});
